c3a6057065a27147df9cedf0aeb5a0d315221bf1
mikok <84585119+miko1034@users.noreply.github.com>
2025-02-13 20:28:47 +0000

    main started with notes

---
+++ main.py
@@ -0,0 +1,6 @@
+#intext:@"yahoo|gmail|hotmail".com filetype:txt site:.uk
+
+
+# initial testing shows that google uses dynamic pages.
+# will most likely have to use selenium
+# will take much longer with using *ugh* selenium 
