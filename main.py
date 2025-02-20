9a54732375cbdc4d0594dd8b44fce00607639cbf
mikok <84585119+miko1034@users.noreply.github.com>
2025-02-20 21:39:57 +0000

    start on main code and another dork

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 #intext:@"yahoo|gmail|hotmail".com filetype:txt site:.uk
+#intext:@"yahoo|gmail|hotmail".com ext:csv | ext:txt
+
+
+def init():
+    #creates config file
+    with open("config.yml", "w+") as f:
+        f.write(f"key: {input('enter api key: ')}")
+        pass
+
 
 
 # initial testing shows that google uses dynamic pages.
