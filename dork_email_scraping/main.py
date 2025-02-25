7a37a6e843c54aedd347d14a524c2a54dc389bc8
miki <84585119+miko1034@users.noreply.github.com>
2025-02-25 15:40:47 +0000

    new solution

--- dork_email_scraping/main.py
+++ dork_email_scraping/main.py
@@ -1,11 +1,20 @@
 import os
 import time
 from dork_email_scraping.func import make_google_search, find_emails, get_all_queries, remove_dupes
+from multiprocessing import Pool, cpu_count
+
+#the loop function to which the query is mapped to in the main functon
+def do_loop(query):
+    print(f"[!!] Using query: {query}")
+    for i in range(0,1000,10):
+        links = make_google_search(i,search_query=query)
+        find_emails(links)
+        print(f"[!] Searched {i} pages of {query}")
+    print(f"[+] Finished searching query: {query}")
 
 
 #main function
 def main():
-
     #creates config file
     if "config.yml" not in os.listdir():
         with open("config.yml", "w+") as f:
@@ -15,19 +24,16 @@ def main():
     if "outputs" not in os.listdir("dork_email_scraping"):
         os.mkdir("dork_email_scraping/outputs")
     
-    # runs the main loop
-    for query in get_all_queries():
-        try:
-            print(f"[!!!] Using query: {query}")
-            for i in range(0,1000,10):
-                links = make_google_search(i, search_query=query)
-                find_emails(links)
-                print(f"[!] Current searched pages: {i+10}")
-                time.sleep(5)
-                print("__Sleeping 5 seconds__")
-        except KeyboardInterrupt:
-            print("Keyboard interrupt detected. Skipping current query...")
-            continue
+    try:
+        # creates a new process for a query. much faster than one process handling all queries seperately.
+        with Pool(cpu_count()) as p:
+            print(f"[!] Starting execution with {cpu_count()} CPUs.")
+            all_queries = get_all_queries()
+            p.map(do_loop,(all_queries))
+    except KeyboardInterrupt:
+        print("Detecting keyboard interrupt. Removing duplicates and quitting...")
+        remove_dupes()
+        quit()
 
     remove_dupes()
     print("Duplicates removed. Quitting...")
